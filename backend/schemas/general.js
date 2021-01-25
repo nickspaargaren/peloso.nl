ba7ac2f82790e029f977bed777e346d54cc7d72e
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-01-25 19:15:21 +0100

    General information toegevoegd

---
+++ backend/schemas/general.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'general',
+  title: 'General',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'lastname',
+      title: 'Lastname',
+      type: 'string',
+    },
+    {
+      name: 'birthdate',
+      title: 'Date of birth',
+      type: 'date',
+    },
+    {
+      name: 'birthplace',
+      title: 'Place of birth',
+      type: 'string',
+    },
+    {
+      name: 'nationality',
+      title: 'Nationality',
+      type: 'string',
+    },
+    {
+      name: 'residence',
+      title: 'Residence',
+      type: 'string',
+    },
+  ]
+}
--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -8,6 +8,7 @@ import languages from './languages.js'
 import work from './work.js'
 import education from './education.js'
 import interests from './interests.js'
+import general from './general.js'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,6 +21,7 @@ export default createSchema({
     languages,
     work,
     education,
-    interests
+    interests,
+    general
   ]),
 })
