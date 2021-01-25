4cb05ea18778f71077fdb1d4598635a9df077202
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-01-25 13:07:24 +0100

    Sanity init

---
+++ backend/schemas/languages.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'languages',
+  title: 'Languages',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+  ]
+}
