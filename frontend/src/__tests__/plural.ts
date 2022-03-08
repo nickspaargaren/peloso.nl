70f1a9899590249bdcf05a1fa7f42e0547c1b308
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-03-08 17:50:03 +0100

    Add plural test

---
+++ frontend/src/__tests__/plural.ts
@@ -0,0 +1,9 @@
+const { plural } = require('../utils');
+
+test('Expect correct plural output with 2 months', () => {
+  expect(plural(2, 'month')).toEqual('2 months');
+});
+
+test('Expect correct plural output with 1 month', () => {
+  expect(plural(1, 'month')).toEqual('1 month');
+});
