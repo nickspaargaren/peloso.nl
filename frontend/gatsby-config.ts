a0fccb11dfd8aae616714943a180df64dbe42e25
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-03-16 20:22:44 +0100

    Update Gatsby config to Typescript - Gatsby 4.9 makes this possible

---
+++ frontend/gatsby-config.ts
@@ -0,0 +1,29 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: 'peloso.nl',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'ozsvr5j0',
+        dataset: 'production',
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+    },
+  ],
+};
+
+export default config;
