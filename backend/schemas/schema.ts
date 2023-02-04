24680bd3cc1bad83f4e96528a60b03348e520dd8
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2023-02-04 11:47:07 +0100

    Update Sanity schemas to ts

--- backend/schemas/schema.ts
+++ backend/schemas/schema.ts
@@ -1,10 +1,10 @@
 // Then import schema types from any plugins that might expose them
-import skills from './skills.js'
-import languages from './languages.js'
-import work from './work.js'
-import education from './education.js'
-import interests from './interests.js'
-import general from './general.js'
+import skills from './skills'
+import languages from './languages'
+import work from './work'
+import education from './education'
+import interests from './interests'
+import general from './general'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default [
